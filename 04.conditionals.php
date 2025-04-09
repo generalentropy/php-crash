a4332828422878be1f39a58d1959e62af24a56cf
entropie <entropie@tuta.io>
2025-04-09 11:37:38 +0200

    loops

--- 04.conditionals.php
+++ 04.conditionals.php
@@ -10,7 +10,7 @@
   <= Less than or equal to
   >= Greater than or equal to
   == Equal to
-  === Identical to
+  === Identical to  // Value + type
   != Not equal to
   !== Not identical to
 */
@@ -23,3 +23,68 @@ if (condition) {
   // code to be executed if condition is true
 }
 */
+
+
+$age = 20;
+
+// if ($age >= 40) {
+//     echo 'yes' ;
+// } else {
+//     echo 'no';
+// };
+
+
+// $t = date('d/m/Y');
+
+$t = date('H');
+
+// print_r($t);
+
+if ($t < 12) {
+    echo 'Good morning';
+} elseif ($t < 17) {
+    echo 'Good afternoon';
+} else {
+    echo 'Good evening';
+};
+
+echo "<br />";
+
+$posts = ['First Blog Post'];
+
+// if (!empty($posts)) {
+//     echo $posts[0];
+// } else {
+//     echo 'No posts';
+// }
+
+
+echo !empty($posts) ? $posts[0] : 'Sorry No posts at this moment';
+
+// $firstPost =  !empty($posts) ? $posts[0] : 'Sorry No posts at this moment';
+
+$firstPost =  $posts[0] ?? null;
+
+echo "<br />";
+
+echo $firstPost;
+
+
+$favcolor  = 'blue';
+
+switch ($favcolor) {
+
+    case 'red':
+        echo 'Your favorite color is red';
+        break;
+
+    case 'green':
+        echo 'Your favorite color is green';
+        break;
+
+    case 'blue':
+        echo 'Your favorite color is blue';
+        break;
+
+    default: echo 'Your favorite color is unknown';
+};
