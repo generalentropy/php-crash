6f7a0aacd077d2b308957fcfe01e55bbf9695641
entropie <entropie@tuta.io>
2025-04-09 14:07:39 +0200

    update

--- 05.loops.php
+++ 05.loops.php
@@ -1,1 +1,20 @@
 <?php
+
+
+
+// for ($x = 0; $x <= 80; $x++) {
+
+//  echo "<p>Counter is : $x</p>";
+
+// };
+
+$x = 1;
+
+
+while ($x <= 15) {
+
+    echo 'Number' . $x  . '<br />';
+
+    $x++;
+
+};
