8ebe4aab6caae250788192f737cb31f4adf3f6e4
entropie <entropie@tuta.io>
2025-04-09 21:21:51 +0200

    update

--- practice/exercice3.php
+++ practice/exercice3.php
@@ -5,7 +5,23 @@
 $prenoms = ['Alice', 'Bob', 'Charlie', 'Diane'];
 
 
-foreach ($prenoms as $user) {
+?>
 
-    echo "<p>Bonjour $user !</p>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+foreach ($prenoms as $user) {
+    echo "<p class='salutation'>Bonjour $user !</p>";
+
 }
+?>  
+</body>
+</html>
